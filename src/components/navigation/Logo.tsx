1e22ad2c2378ff1c7f29a48f16fd9c633e1d5fab
Heeran Kim <heerankim3@gmail.com>
2025-05-14 10:43:24 +1000

    Fixed logo component

--- src/components/navigation/Logo.tsx
+++ src/components/navigation/Logo.tsx
@@ -8,10 +8,10 @@ export default function Logo() {
       <Image
         src="/AKA.png"
         alt="Logo"
-        width="0" // Set width to 0 to let Next.js handle image size with CSS
-        height="0" // Set height to 0 to allow CSS to control the size
-        className="w-[80px] h-auto" // Use Tailwind CSS to set width to 80px and maintain aspect ratio with auto height
-        priority // Added priority to optimize LCP
+        width={80}
+        height={40}
+        className="w-[80px] h-auto"
+        priority
       />
     </Link>
   );
